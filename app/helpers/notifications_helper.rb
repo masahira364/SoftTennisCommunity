f0456eae6539ce1e9eacaea834e4a2e67e132e1e
vagrant <vagrant@localhost.localdomain>
2020-08-31 04:36:07 +0000

    fix_header

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,5 +1,10 @@
 module NotificationsHelper
   def unchecked_notifications
-    @notifications = current_user.passive_notifications.where(checked: false)
+    @notifications = current_user.team.passive_team_notifications.where(checked: false, action: "team").
+     				 or(current_user.team.passive_team_notifications.where(checked: false, action: "withdrawal")).
+     				 or(current_user.team.passive_team_notifications.where(checked: false, action: "article")).
+     				 or(current_user.team.passive_team_notifications.where(checked: false, action: "event")).
+     				 or(current_user.passive_notifications.where(checked: false)).
+     				 where.not(visitor_id: current_user.id)
   end
 end
