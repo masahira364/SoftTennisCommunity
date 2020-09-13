064b33a999a003f8d96215bb021d6bdff09200cf
vagrant <vagrant@localhost.localdomain>
2020-09-13 16:57:23 +0000

    update_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    comment = current_user.comments.new(comment_params)
-    comment.article_id = @article.id
-    if comment.save
-      comment.create_notification_comment(current_user, comment.id)
+    @comment = current_user.comments.new(comment_params)
+    @comment.article_id = @article.id
+    if @comment.save
+      @comment.create_notification_comment(current_user, @comment.id)
       redirect_to request.referer, notice: "コメントを投稿しました"
     else
-      @article = Article.find(params[:article_id])
-      flash[:alert] = "コメントの投稿に失敗しました。コメント欄に文字を入力してください。"
-      redirect_to request.referer
+      render :error
     end
   end
 
